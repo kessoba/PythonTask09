b8294814ac7baaec450f41ffe1f29b56a391067c
kessoba <mariamabahdiallo1@gmail.com>
2024-09-11 21:33:46 +0000

    test additon et dvision

--- Test/test_my_functions.py
+++ Test/test_my_functions.py
@@ -1,5 +1,20 @@
 import pytest
-import Source.MyFonctions as myfonctions
+import  source.my_functions as my_functions
 
 def test_add():
-    pass
+    result = my_functions.add(3, 6)
+    assert result == 9
+
+def test_add_strings():
+    result=my_functions.add('i like ',"burgers")
+    assert result =="i like burgers"
+
+
+def test_divided():
+    result=my_functions.divided(10,5)
+    assert result==2
+
+def test_divided_byZero():
+    with pytest.raises(ValueError):
+        my_functions.divided(10,0)
+
